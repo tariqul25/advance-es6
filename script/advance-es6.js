e4ec6cbe3a13d85683fda36e782374c3a5b94fbf
ImTariquL <imtariqul21@gmail.com>
2025-06-01 21:06:46 +0600

    optional chaining

---
+++ script/advance-es6.js
@@ -0,0 +1,22 @@
+// -----------------------------Advance ES6------------------------------------------------------
+
+// --------------------------------Optinal Chaining---------------------------------------
+
+// optional chaining hocche kuno nested object e nested obj name wrong dilew jeno error na diye tar akta process.Ai jonno Object value access korar somoy obj name er shesh e ?(question mark) used korle ta shudu undefine dibe kuno error dibe na
+
+const person = {
+  name: "tariqul",
+  age: 21,
+  roll: 676819,
+  institute: {
+    name: "kpi",
+    departement: "cst",
+    time: {
+      shift: "first",
+    },
+  },
+};
+
+// console.log(person.institute.timing.shift);  //person object e kuno timing naam e kuno nested object naii.tai error dichhe
+// console.log(person.institute.timing?.shift); //ekane nested obj name er sathe ? use koraii error dicche na shudu undefine dicche
+
